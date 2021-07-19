83a2311699c9e279ef9a62409974885793a6305b
Rob Quatela <rob.quatela@gmail.com>
2021-07-19 10:57:22 -0400

    adjusting for name and location

--- handler.js
+++ handler.js
@@ -2,10 +2,10 @@ const puppeteer = require('puppeteer');
 
 /**
  * scrapes twitter profile to get name, avatar, follower count, following count, join date, Twitter handle, and location
- * @param {string} profile - name of twitter profile in url
- * @return {{name: string, avatarUrl: string, followerCount: string, followingCount: string, joinAt: string, handle: string, location: string, description: string}}
+ * @param {string} handle - name of twitter profile in url
+ * @return {{name: string, avatarUrl: string, followerCount: string, followingCount: string, joinAt: string, handle: string, description: string}}
  */
-const scrapeTwitterProfile = async (profile) => {
+const scrapeTwitterProfile = async (handle) => {
 
   const browser = await puppeteer.launch({
     headless: false,
@@ -19,7 +19,7 @@ const scrapeTwitterProfile = async (profile) => {
   });
 
   const page = await browser.newPage();
-  await page.goto(`https://twitter.com/${profile}`);
+  await page.goto(`https://twitter.com/${handle}`);
   await page.waitForSelector('div.css-1dbjc4n.r-1ifxtd0.r-ymttw5.r-ttdzmv', {timeout: 10000});
 
   if (process.env.ENV === 'development')
@@ -34,30 +34,32 @@ const scrapeTwitterProfile = async (profile) => {
       .querySelector('img')
       .src;
 
+    const name = Array.from(app.querySelector('div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1777fci').querySelector('h2').querySelectorAll('span')).find(x => x.innerText !== null).innerText;
+
     const handle = Array.from(profileEl.querySelectorAll('span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0')).find(x => x.innerText.includes('@')).innerText;
 
-    const description = profileEl.querySelector('[data-testid="UserDescription"]').querySelector('span').innerText;
+    const descriptionEl = profileEl.querySelector('[data-testid="UserDescription"]');
+    const description = descriptionEl !== null ? descriptionEl.querySelector('span').innerText : 'Description not available';
 
     const profileHeader = profileEl.querySelector('[data-testid="UserProfileHeader_Items"]');
 
-
-    const location = Array.from(profileHeader.querySelectorAll('span')).find(x => x.innerText.includes(',')).innerText;
     const joinedAt = Array.from(profileHeader.querySelectorAll('span')).find(x => x.innerText.includes('Joined')).innerText;
 
     const followingLink = Array.from(app.querySelectorAll('a')).find(x => x.href.includes('following'));
-    const following = Array.from(followingLink.querySelectorAll('span')).find(x => x.innerText !== null).innerText;
+    const followingCount = Array.from(followingLink.querySelectorAll('span')).find(x => x.innerText !== null).innerText;
 
     const followersLink = Array.from(app.querySelectorAll('a')).find(x => x.href.includes('followers'));
-    const followers = Array.from(followersLink.querySelectorAll('span')).find(x => x.innerText !== null).innerText;
+    const followerCount = Array.from(followersLink.querySelectorAll('span')).find(x => x.innerText !== null).innerText;
 
     return JSON.stringify({
       handle,
       avatarUrl,
       description,
-      location,
+      name,
       joinedAt,
-      following,
-      followers,
+      followingCount,
+      followerCount,
+      url: document.URL,
     });
   });
 
@@ -65,7 +67,6 @@ const scrapeTwitterProfile = async (profile) => {
 
   return {
     ...JSON.parse(profileResult),
-    name: profile,
   };
 }
 
--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const handler = require('./handler');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
+
 
 /**
  * testing web server to ensure it works on your machine
@@ -17,11 +21,16 @@ app.get('/ping', (req, res) => {
 
 /**
  * route used to lookup and scrape Twitter profile
- * @param {string} profile - profile name inside of Twitter URL
+ * @param {string} handle - handle inside of Twitter URL
  */
-app.post('/profiles/:profile', async (req, res) => {
-  const result = await handler.scrapeTwitterProfile(req.params.profile);
-  res.send(result);
+app.post('/handles/:handle', async (req, res, next) => {
+  try {
+    const result = await handler.scrapeTwitterProfile(req.params.handle);
+    res.send(result);
+  } catch (e) {
+    next(e)
+  }
+
 });
 
 
